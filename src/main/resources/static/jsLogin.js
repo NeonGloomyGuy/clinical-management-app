74a7cd5a68f18479ac6562fe107fa9024b8573f4
FatalShoro <chito_conter17@outlook.com>
2025-05-01 20:44:47 -0700

    Login completely created

---
+++ src/main/resources/static/jsLogin.js
@@ -0,0 +1,61 @@
+const userCtn = document.getElementById('user_ctn');
+const passCtn = document.getElementById('pass_ctn');
+
+const user = document.getElementById('input_user');
+const pass = document.getElementById('input_pass');
+
+const errorMessage = document.getElementById('error_message');
+
+document.getElementById('verify_btn').addEventListener('click', () => {
+    
+    if(user.value === "shoro" && pass.value === "1234") {
+        window.location.href = "main.html";
+    } else {
+        
+        if(user.value !== "shoro") {
+            userCtn.style.backgroundColor = "rgb(240, 123, 125)";
+            user.style.backgroundColor = "rgb(240, 123, 125)";
+            errorMessage.innerText = "¡Usuario Incorrecto!";
+            errorMessage.style.display = "inline";
+        }
+
+        if(pass.value !== "1234") {
+            passCtn.style.backgroundColor = "rgb(240, 123, 125)";
+            pass.style.backgroundColor = "rgb(240, 123, 125)";
+            errorMessage.innerText = "¡Contraseña Incorrecta!";
+            errorMessage.style.display = "inline";
+        }
+
+        if(user.value !== "shoro" && pass.value !== "1234") {
+            errorMessage.innerText = "¡Usuario y Contraseña Incorrectos!";
+            errorMessage.style.display = "inline";
+        }
+
+    }
+    
+});
+
+user.addEventListener("focus", function() {
+    userCtn.style.backgroundColor = "white";
+    user.style.backgroundColor = "white";
+    passCtn.style.backgroundColor = "white";
+    pass.style.backgroundColor = "white";
+});
+
+pass.addEventListener("focus", function() {
+    userCtn.style.backgroundColor = "white";
+    user.style.backgroundColor = "white";
+    passCtn.style.backgroundColor = "white";
+    pass.style.backgroundColor = "white";
+});
+
+// ERASE INPUT VALUE BUTTON
+
+const eraseInputValueBtn = document.querySelectorAll('.erase_input_value_btn');
+
+eraseInputValueBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        user.value = "";
+        pass.value = "";
+    });
+});
